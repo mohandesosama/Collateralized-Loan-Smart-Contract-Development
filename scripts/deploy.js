a7a4cddda04274e8795b816cc70fc55464baf014
mohandesosama <mohandesosama@yahoo.com>
2024-05-06 17:11:56 -0700

    can't fix the error

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,2 +1,18 @@
-const hre=require("hardhat")
-console.log(hre)
+const {hre}=require("hardhat")
+async function main()
+{
+    //const lockedAmount= hre.ethers.utils.parseEther("1");
+    const amount = hre.ethers.utils.parseEther("1.0");
+    //console.log(lockedAmount);
+    console.log(amount)
+}
+// Call the main function to start the deployment process
+main()
+  .then(() => {
+    console.log("Deployment completed successfully.");
+    process.exit(0);
+  })
+  .catch(error => {
+    console.error("Error during deployment:", error);
+    process.exit(1);
+  });
